c13841ab0eba154a26b1ea713657cd59748743a5
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-11-16 20:05:53 +0100

    Add board's cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-const Cell = () => {};
+const Cell = ({ isSnake, isFood }) => {
+    let classNaeme = "cell";
+    if (isSnake) {
+        classNaeme += " snake";
+    } 
+    if (isFood) {
+        classNaeme += " food";
+    }
+    return <div className={classNaeme}></div>;
+};
 
 export default Cell;
