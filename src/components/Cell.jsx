983f2cf62d0b1e592f573de1de291b30bf2d238f
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2025-11-16 20:35:47 +0100

    Update stiles, applying tailwindcss

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Cell = ({ isSnake, isFood }) => {
-    let classNaeme = "cell";
-    if (isSnake) {
-        classNaeme += " snake";
-    } 
-    if (isFood) {
-        classNaeme += " food";
-    }
-    return <div className={classNaeme}></div>;
+    let base = "w-5 h-5";
+    
+    if (isSnake) base += " bg-green-500";
+    else if (isFood) base += " bg-pink-500 rounded-full";
+    else base += " bg-gray-300";
+
+    
+    return <div className={base}></div>;
 };
 
 export default Cell;
